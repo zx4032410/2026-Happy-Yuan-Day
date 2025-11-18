b200c920b003ecb0a33787df18b362e1056986d6
zx40324 <zx4032410@gmail.com>
2025-11-18 17:29:46 +0800

    新增 SEO 標籤以改善網站可見性，並將多個問題文本翻譯為英文以增強遊戲的可訪問性和國際化。

--- questions.js
+++ questions.js
@@ -49,7 +49,7 @@ const QUESTION_BANK = [
             incorrectAnswers: ["台北市", "台中市"]
         },
         'en': {
-            question: "媛媛來自台灣的哪個城市?",
+            question: "Which city in Taiwan is Yuan from?",
             correctAnswer: "高雄市",
             incorrectAnswers: ["台北市", "台中市"]
         }
@@ -62,7 +62,7 @@ const QUESTION_BANK = [
             incorrectAnswers: ["《OH!(中文版)》", "《一起走》"]
         },
         'en': {
-            question: "以下哪首歌不是媛媛有參與作詞的",
+            question: "Which of the following songs was not co-written by Yuan?",
             correctAnswer: "《SO WHAT》",
             incorrectAnswers: ["《OH!(中文版)》", "《一起走》"]
         }
@@ -75,9 +75,9 @@ const QUESTION_BANK = [
             incorrectAnswers: ["每天都賴床", "會在床上吃零食"]
         },
         'en': {
-            question: "在娛樂超skr中，媛媛被團員爆料了哪一點",
-            correctAnswer: "私服穿搭最特別",
-            incorrectAnswers: ["每天都賴床", "會在床上吃零食"]
+            question: "In 娛樂超skr, what did the members reveal about Yuan?",
+            correctAnswer: "Her casual outfits are the most unique",
+            incorrectAnswers: ["Wakes up late every day", "Eats snacks in bed"]
         }
     },
     {
@@ -88,9 +88,9 @@ const QUESTION_BANK = [
             incorrectAnswers: ["于哥", "當然都喜歡啊"]
         },
         'en': {
-            question: "媛媛在1/18演唱會的時候表示金姐跟于哥哪個人她比較喜歡？",
+            question: "At the 1/18 concert, who did Yuan say she preferred between 金姐 and 于哥?",
             correctAnswer: "金姐",
-            incorrectAnswers: ["于哥", "當然都喜歡啊"]
+            incorrectAnswers: ["于哥", "Of course, I like both!"]
         }
     },
     {
@@ -101,9 +101,9 @@ const QUESTION_BANK = [
             incorrectAnswers: ["空服員", "寵物店員工"]
         },
         'en': {
-            question: "媛媛曾經在娛樂百分百的訪談中說過不當偶像想當什麼職業？",
-            correctAnswer: "海生館員工",
-            incorrectAnswers: ["空服員", "寵物店員工"]
+            question: "In an interview on 娛樂百分百, what profession did Yuan say she would pursue if not an idol?",
+            correctAnswer: "Aquarium staff",
+            incorrectAnswers: ["Flight attendant", "Pet shop employee"]
         }
     },
     {
@@ -114,7 +114,7 @@ const QUESTION_BANK = [
             incorrectAnswers: ["大安站", "忠孝新生站"]
         },
         'en': {
-            question: "這次的生日咖啡廳靠近台北捷運哪個站？",
+            question: "Which Taipei MRT station is this birthday cafe near?",
             correctAnswer: "信義安和站",
             incorrectAnswers: ["大安站", "忠孝新生站"]
         }
@@ -127,8 +127,8 @@ const QUESTION_BANK = [
             incorrectAnswers: ["LivingRoom客廳咖啡", "耍廢茶"]
         },
         'en': {
-            question: "以下哪個是這次生日應援禮包的發送地點？",
-            correctAnswer: "以上皆是",
+            question: "Which of the following is a distribution location for this birthday support gift package?",
+            correctAnswer: "All of the above",
             incorrectAnswers: ["LivingRoom客廳咖啡", "耍廢茶"]
         }
     },
@@ -140,9 +140,9 @@ const QUESTION_BANK = [
             incorrectAnswers: ["18歲", "20歲"]
         },
         'en': {
-            question: "今年是媛媛的幾歲生日？",
-            correctAnswer: "21歲",
-            incorrectAnswers: ["18歲", "20歲"]
+            question: "How old is Yuan celebrating this birthday?",
+            correctAnswer: "21 years old",
+            incorrectAnswers: ["18 years old", "20 years old"]
         }
     },
     {
@@ -153,7 +153,7 @@ const QUESTION_BANK = [
             incorrectAnswers: ["我是真的真的很愛你", "我是真的很喜歡你"]
         },
         'en': {
-            question: "11/08的演唱會 媛媛演唱的Solo曲是什麼？",
+            question: "What was Yuan's solo song at the 11/08 concert?",
             correctAnswer: "我是真的很愛很愛你",
             incorrectAnswers: ["我是真的真的很愛你", "我是真的很喜歡你"]
         }
@@ -166,9 +166,9 @@ const QUESTION_BANK = [
             incorrectAnswers: ["漫威英雄系列", "神鬼奇航系列"]
         },
         'en': {
-            question: "媛媛曾經提過喜歡哪個電影系列？",
-            correctAnswer: "哈利波特系列",
-            incorrectAnswers: ["漫威英雄系列", "神鬼奇航系列"]
+            question: "Which movie series did Yuan mention she likes?",
+            correctAnswer: "Harry Potter series",
+            incorrectAnswers: ["Marvel Cinematic Universe", "Pirates of the Caribbean series"]
         }
     }
     // TODO: 在這裡新增更多題目！
